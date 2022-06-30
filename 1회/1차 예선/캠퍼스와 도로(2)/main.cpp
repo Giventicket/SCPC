77163b160244229d13687a88d79e558a453c3dfe
seo jun pyo <jpseo99@naver.com>
2022-06-30 16:21:35 +0900

    [add] 캠퍼스와 도로(2)

--- 1회/1차 예선/캠퍼스와 도로(2)/main.cpp
+++ 1회/1차 예선/캠퍼스와 도로(2)/main.cpp
@@ -9,22 +9,80 @@
 
 using namespace std;
 
+int N, M;
+vector<vector<tuple<int, int>>> adj;
+vector<int> cnt;
+set<int> answer;
+
+void countShortestPath(int source){
+	cnt = vector<int>(N+1, 0);
+	priority_queue<tuple<int, int>> pq;
+	vector<int> dist = vector<int>(N+1, INF);
+	dist[source] = 0;
+	pq.push({-dist[source], source});
+	while(!pq.empty()){
+		int curW = -get<0>(pq.top());
+		int cur = get<1>(pq.top());
+		pq.pop();
+		if(dist[cur] < curW)
+			continue;
+		cnt[cur]++;
+		for(tuple<int, int> ele: adj[cur]){
+			int next = get<0>(ele);
+			int weight = get<1>(ele);
+			if(dist[next] >= curW + weight){
+				dist[next] = curW + weight;
+				pq.push({-dist[next], next});
+			}
+		}
+	}
+	return;
+}
+
+void renewAnswer(int source){
+	countShortestPath(source);
+	priority_queue<tuple<int, int, int>> pq;
+	vector<int> dist = vector<int>(N+1, INF);
+	dist[source] = 0;
+	pq.push({-dist[source], source, -1});
+	while(!pq.empty()){
+		int curW = -get<0>(pq.top());
+		int cur = get<1>(pq.top());
+		int parent = get<2>(pq.top());
+		pq.pop();
+		if(dist[cur] < curW)
+			continue;
+		if(cur != source && parent != source && cnt[cur] == 1)
+			answer.insert(parent);
+		for(tuple<int, int> ele: adj[cur]){
+			int next = get<0>(ele);
+			int weight = get<1>(ele);
+			if(dist[next] >= curW + weight){
+				dist[next] = curW + weight;
+				pq.push({-dist[next], next, cur});
+			}
+		}
+	}
+	return;
+}
+
 int main(){
-	freopen("input.txt", "r", stdin);
+	//freopen("input.txt", "r", stdin);
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);  
 	cout.tie(NULL);
 	
-	int T, N, M;
+	int T;
 	cin >> T;	
 	
 	for(int t = 1; t <= T; t++){
 		cout << "Case #" << t << "\n";
 		cin >> N >> M;
 	
-		vector<vector<tuple<int, int>>> adj = vector<vector<tuple<int, int>>>(N+1);
-		
 		// inputs		
+		answer.clear();
+		adj = vector<vector<tuple<int, int>>>(N+1);
+		
 		for(int i = 0; i < M; i++){
 			int a, b, c;
 			cin >> a >> b >> c;
@@ -33,6 +91,13 @@ int main(){
 		}
 		
 		// outputs
+		for(int source = 1; source <= N; source++)
+			renewAnswer(source);
+		
+		cout << answer.size() << " ";
+		for(int ele: answer)
+			cout << ele << " ";
+		cout << "\n";		
 	}
 	
 	return 0;
