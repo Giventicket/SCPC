8ae3573f2831664f462b76de57bd21fd623a226b
seo jun pyo <jpseo99@naver.com>
2022-07-02 01:46:12 +0900

    [add] 범위 안의 숫자 초안

---
+++ 6회/1차 예선/범위 안의 숫자/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	freopen("input.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);  
+	cout.tie(NULL);
+	
+	int T;
+	cin >> T;
+	
+	for(int t = 1; t <= T; t++){
+		// inputs
+		cout << "Case #" << t << "\n";
+		
+		// outputs
+	}
+	
+	return 0;
+}
