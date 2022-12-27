346f0cb4251b841df30bde473879965afdd82f0a
junpyoseo <jpseo99@naver.com>
2022-12-27 20:43:46 +0900

    add

--- 6회/2차 예선/고구마/main.cpp
+++ 6회/2차 예선/고구마/main.cpp
@@ -1,58 +1,35 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <cstring>
+#include <set>
 
 using namespace std;
 
-#define MAXN 200001
 int N;
-vector<long long int> A, B;
-long long int dp[MAXN][5];
+long long int M;
+vector<long long int> price;
 
 void input() {
-	memset(dp, 0, sizeof(dp));
-	cin >> N;
-	A = vector<long long int>(N);
-	B = vector<long long int>(N);
+	cin >> N >> M;
+	price.clear();
+	price.resize(N);
 	for (int i = 0; i < N; i++)
-		cin >> A[i];
-	for (int i = 0; i < N; i++)
-		cin >> B[i];
-	sort(A.begin(), A.end());
-	sort(B.begin(), B.end());
+		cin >> price[i];
 	return;
 }
 
-void solve(){
-	dp[0][0] = abs(A[0]-B[0]);
-	
-	dp[1][0] = dp[0][0] + abs(A[1]-B[1]);
-	dp[1][1] = abs(A[1]-B[0]);
-	dp[1][2] = abs(A[0]-B[1]);
-	dp[1][3] = dp[0][0];
-	dp[1][4] = min(min(dp[1][0], dp[1][1]), min(dp[1][2], dp[1][3]));
-	dp[1][4] = min(dp[1][4], dp[0][4] + abs(A[1]-B[1]));
-	
-	for (int i = 1; i < N-1; i++) {
-		dp[i+1][0] = dp[i][0] + abs(A[i+1]-B[i+1]);		
-		
-		dp[i+1][1] = min(
-			dp[i-1][0] + abs(A[i+1]-B[i]), 
-			dp[i][1] + abs(A[i+1]-B[i])
-		);
-		
-		dp[i+1][2] = min(
-			dp[i-1][0] + abs(A[i]-B[i+1]), 
-			dp[i][2] + abs(A[i]-B[i+1])
-		);
-		
-		dp[i+1][3] = dp[i][0];
-		
-		dp[i+1][4] = min(min(dp[i+1][0], dp[i+1][1]), min(dp[i+1][2], dp[i+1][3]));
-		dp[i+1][4] = min(dp[i+1][4], dp[i][4] + abs(A[i+1]-B[i+1]));
+long long int solve(){
+	long long int answer = 0;
+	set<long long int> st;
+	long long int currentSum = 0;
+	st.insert(0);
+	for(int i = 0; i < N; i++) {
+		currentSum += price[i];
+		st.insert(currentSum);
+		set<long long int>::iterator it = st.lower_bound(currentSum - M);
+		answer = max(answer, currentSum - (*it));
 	}
-	return;
+	return answer;
 }
 
 int main(){
@@ -68,8 +45,7 @@ int main(){
 		// inputs
 		cout << "Case #" << t << "\n";
 		input();
-		solve();
-		cout << dp[N-1][4] << "\n";
+		cout << solve() << "\n";
 	}
 	
 	return 0;
