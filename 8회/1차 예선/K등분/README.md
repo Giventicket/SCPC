18f6a4b9a29729f56b3f5d3ecca5f7c67f822cb2
junpyo.seo <jpseo99@naver.com>
2023-02-07 17:24:46 +0900

    add

---
+++ 8회/1차 예선/K등분/README.md
@@ -0,0 +1,5 @@
+## K등분 insight sketch
+
+<p align="center">
+  <img src=""/>
+</p>
---
+++ 8회/1차 예선/K등분/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long int ll;
+
+void input() {	
+	return;
+}
+
+void solve() {
+	return;
+}
+
+int main(){
+	freopen("input.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);  
+	cout.tie(NULL);
+	
+	int T;
+	cin >> T;
+	
+	for(int t = 1; t <= T; t++){
+		cout << "Case #" << t << "\n";
+		input();
+		solve();
+	}
+	
+	return 0;
+}
